8181590e414e5aa2872c2484620d8af2fc57fbc7
Dejwid <s205915@sggw.edu.pl>
2023-08-29 12:23:28 +0200

    Interdependent equation estimation

---
+++ Lab_04/interdependent_equation_estimation.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import statsmodels.api as sm
+data = pd.read_excel('data_03.xlsx', sheet_name = 'dane')
+
+Y1 = data['y1t']
+Y2 = data['y2t']
+Z1 = data['z1t']
+Z2 = data['z2t']
+Z3 = data['z3t']
+Z = data[['z1t', 'z2t', 'z3t']]
+
+
+model_01 = sm.OLS(Y1, Z).fit()
+print(model_01.summary())
+
+model_02 = sm.OLS(Y2, Z).fit()
+print(model_02.summary())
+
+data['predicted_y1'] = model_01.predict()
+data['predicted_y2'] = model_02.predict()
+
+
+X1 = data[['predicted_y2', 'z1t']]
+model_03 = sm.OLS(Y1, X1).fit()
+print(model_03.summary())
+
+X2 = data[['predicted_y1', 'z2t', 'z3t']]
+model_04 = sm.OLS(Y2, X2).fit()
+print(model_04.summary())
+
