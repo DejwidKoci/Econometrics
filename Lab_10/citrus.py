0e61bcf6cf9cdf905bdc553074f9dc69ee180106
Dejwid <s205915@sggw.edu.pl>
2023-08-31 11:48:20 +0200

    citrus

--- Lab_10/citrus.py
+++ Lab_10/citrus.py
@@ -3,11 +3,11 @@ import statsmodels.api as sm
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_excel("warzywa.xlsx", sheet_name = 'Dane')
+data = pd.read_excel('owoce.xlsx', sheet_name = 'Dane')
 
 # power model
-Y1 = np.log(data['y'])
-X1 = np.log(data['x'])
+Y1 = np.log(data['cytrusowe'])
+X1 = np.log(data['X_real'])
 X1 = sm.add_constant(X1)
 
 model_01 = sm.OLS(Y1, X1).fit()
@@ -15,23 +15,22 @@ print(model_01.summary())
 print()
 print("Współczynniki funkcji potęgowej  y = a * x ** b")
 print("a: ", np.exp(model_01.params['const']))
-print("b: ", model_01.params['x'])
+print("b: ", model_01.params['X_real'])
 print()
 print("INTERPRETACJE: ")
-print("Wzrost miesięcznych dochodów o 1% powoduje wzrost miesięcznych wydatków na warzywa i ich przetwory o 0.58%")
+print("Wzrost miesięcznych dochodów o 1% powoduje wzrost miesięcznego spożycia owoców cyrusowych o 0.55% ")
 
 flexibility = pd.DataFrame({
-    "x": data['x'],
-    "Elastyczność": model_01.params['x']
+    "x": data['X_real'],
+    "Elastyczność": model_01.params['X_real']
 })
 print()
 print("ELASTYCZNOŚCI: ")
 print(flexibility)
 
-y_predicted = np.exp(model_01.params['const']) * data['x'] ** model_01.params['x']
+y_predicted = np.exp(model_01.params['const']) * data['X_real'] ** model_01.params['X_real']
 plt.figure(figsize = (10, 6))
-plt.scatter(data['x'], data['y'], label = 'Y', marker = 'o')
-plt.plot(data['x'], y_predicted)
+plt.scatter(data['X_real'], data['cytrusowe'], label = 'Y', marker = 'o')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title("Linearyzacja modelu potęgowego")
@@ -39,29 +38,30 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+
 # exponential with reverse
 Y2 = Y1
-X2 = 1 / data['x']
+X2 = 1 / data['X_real']
 X2 = sm.add_constant(X2)
 
 model_02 = sm.OLS(Y2, X2).fit()
 print(model_02.summary())
 saturation = np.exp(model_02.params['const'])
-vertex = -model_02.params['x'] / 2
+vertex = -model_02.params['X_real'] / 2
 
 print()
 print("Współczynniki funkcji wykładniczej z odwrotnością  y = exp(a - b/x)")
 print("a: ", model_02.params['const'])
-print("b: ", model_02.params['x'])
+print("b: ", model_02.params['X_real'])
 print("Poziom nasycenia: ", saturation)
 print()
 print("INTERPRETACJE: ")
-print(f"Wraz ze wzrostem dochodów zwiększały się wydatki na warzywa ale nie przekraczały {round(saturation, 2)}%")
-print(f"Na początku wraz ze wzrostem dochodów zwiększały się wydatki na warzywa w przyspieszonym tempie po czym po przekroczeniu dochodów na poziomie {round(vertex, 2)} wydatki na warzywa zaczeły wzrastać wolniej")
+print(f"Wraz ze wzrostem dochodów zwiększało się spożycie owoców cytrusowych ale nie przekraczały one {round(saturation, 2)}%")
+print(f"Na początku wraz ze wzrostem dochodów zwiększało się spożycie owoców cytrusowych w przyspieszonym tempie po czym po przekroczeniu dochodów na poziomie {round(vertex, 2)} spożycie owoców zaczeły wzrastać wolniej")
 
 flexibility = pd.DataFrame({
-    "x": data['x'],
-    "Elastyczność": -model_02.params['x'] / data['x']
+    "x": data['X_real'],
+    "Elastyczność": -model_02.params['X_real'] / data['X_real']
 })
 print()
 print("ELASTYCZNOŚCI: ")
@@ -69,10 +69,9 @@ print(flexibility)
 
 
 
-y_predicted = np.exp(model_02.params['const'] + model_02.params['x'] / data['x'])
+y_predicted = np.exp(model_02.params['const'] + model_02.params['X_real'] / data['X_real'])
 plt.figure(figsize = (10, 6))
-plt.scatter(data['x'], data['y'], label = 'Y', marker = 'o')
-plt.plot(data['x'], y_predicted)
+plt.scatter(data['X_real'], data['cytrusowe'], label = 'Y', marker = 'o')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title("Linearyzacja modelu wykładniczego z odwrotnością")
@@ -82,35 +81,34 @@ plt.show()
 
 # Tornqvist
 # y = ax / (b + x)
-Y3 = 1 / data['y']
-X3 = 1 / data['x']
+Y3 = 1 / data['cytrusowe']
+X3 = 1 / data['X_real']
 X3 = sm.add_constant(X3)
 
 model_03 = sm.OLS(Y3, X3).fit()
 print(model_03.summary())
 
 a = 1 / model_03.params['const']
-b = a * model_03.params['x']
+b = a * model_03.params['X_real']
 print()
 print("Współczynniki funkcji Tornqvista y = ax / (b + x)")
 print("a (poziom nasycenia): ", a)
 print("b: ", b)
 print()
 print("INTERPRETACJE: ")
-print(f"Wraz ze wzrostem dochodów zwiększały się wydatki na warzywa ale nie przekraczały {round(a, 2)}%")
+print(f"Wraz ze wzrostem dochodów zwiększało się spożycie owoców cytrusowych ale nie przekraczały {round(a, 2)}%")
 
 flexibility = pd.DataFrame({
-    "x": data['x'],
-    "Elastyczność": model_03.params['x'] / (model_03.params['x'] + data['x'])
+    "x": data['X_real'],
+    "Elastyczność": model_03.params['X_real'] / (model_03.params['X_real'] + data['X_real'])
 })
 print()
 print("ELASTYCZNOŚCI: ")
 print(flexibility)
 
-y_predicted = a * data['x']/(b + data['x'])
+y_predicted = a * data['X_real']/(b + data['X_real'])
 plt.figure(figsize = (10, 6))
-plt.scatter(data['x'], data['y'], label = 'Y', marker = 'o')
-plt.plot(data['x'], y_predicted)
+plt.scatter(data['X_real'], data['cytrusowe'], label = 'Y', marker = 'o')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title("Linearyzacja modelu Tornqvista")
